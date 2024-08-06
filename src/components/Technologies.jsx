faea77725296297348dcfeff4d69e5c4d5738001
liug88 <gavinliu368@gmail.com>
2024-08-05 18:05:19 -0700

    first commit

---
+++ src/components/Technologies.jsx
@@ -0,0 +1,81 @@
+import { RiReactjsLine } from "react-icons/ri";
+import { TbBrandNextjs } from "react-icons/tb";
+import { SiMongodb } from "react-icons/si";
+import { FaPython, FaJava} from "react-icons/fa";
+import { motion } from "framer-motion"; 
+
+const iconVariants = (duration) => ({
+    initial: {y: -10},
+    animate: {
+        y: [10, -10],
+        transition: {
+            duration: duration,
+            ease: "linear",
+            repeat: Infinity,
+            repeatType: "reverse",
+        }
+    },
+
+})
+
+const Technologies = () => {
+  return (
+    <div className="border-b border-neutral-800 pb-24">
+        <motion.h1 
+        whileInView={{opacity: 1, y: 0}}
+        initial={{opacity: 0, y: -100}}
+        transition={{duration: 1.5}}
+        className="my-20 text-center text-4xl"> Technologies 
+        </motion.h1>
+        <motion.div 
+        whileInView={{opacity: 1, x: 0}}
+        initial={{opacity: 0, x: -100}}
+        transition={{duration: 1.5}}
+        className="flex flex-wrap items-center justify-center gap-4">
+            <motion.div 
+            variants={iconVariants(1.5)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+                <RiReactjsLine className="text-7xl text-cyan-400" />
+                <p className="text-center">React</p>
+            </motion.div>
+            <motion.div 
+            variants={iconVariants(3)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+                <TbBrandNextjs className="text-7xl" />
+                <p className="text-center">Next.js</p>
+            </motion.div>
+            <motion.div 
+            variants={iconVariants(5)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+                <SiMongodb className="text-7xl text-green-500" />
+                <p className="text-center">MongoDB</p>
+            </motion.div>
+            <motion.div 
+            variants={iconVariants(2)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+                <FaPython className="text-7xl text-sky-700" />
+                <p className="text-center">Python</p>
+            </motion.div>
+            <motion.div 
+            variants={iconVariants(6)}
+            initial="initial"
+            animate="animate"
+            className="rounded-2xl border-4 border-neutral-800 p-4">
+                <FaJava className="text-7xl text-red-500" />
+                <p className="text-center">Java</p>
+            </motion.div>
+        </motion.div>
+      
+    </div>
+  )
+}
+
+export default Technologies;
